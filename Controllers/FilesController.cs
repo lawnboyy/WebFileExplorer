9725461057aa158c3ef07466a295b2cd3b45c0d2
Alan A. Hunt <aahunt@redstoneconsultinggroup.org>
2020-12-28 09:26:22 -0600

    Rename template controller and model class

---
+++ Controllers/FilesController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebFileExplorer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilesController : ControllerBase
+    {
+        private static readonly string[] _files = new[]
+        {
+            "file1.txt", "doc3.docx", "test.txt", "Presentation.ppt"
+        };
+
+        private readonly ILogger<FilesController> _logger;
+
+        public FilesController(ILogger<FilesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<File> Get()
+        {
+            var rng = new Random();
+      return _files.Select(f => new File
+            {
+             Name  = f
+            })
+            .ToArray();
+        }
+    }
+}

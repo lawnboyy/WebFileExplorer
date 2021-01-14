9b0697df1a6dede8cd3661b5ba9aa40eb9304e07
Alan A. Hunt <alan.hunt@1st-edge.com>
2021-01-14 11:25:24 -0600

    11: Use dictionaries for encoding and decoding
    This will fail for the % sign in a directory name since it is used to escape all
    characters. It double encodes and the server throws an error.

--- wwwroot/scripts/utilities/urlUtility.js
+++ wwwroot/scripts/utilities/urlUtility.js
@@ -1,5 +1,27 @@
-﻿// TODO: Build a dictionary of UTF-8 encodings to use here instead of
-// applying just spaces and back slashes.
+﻿// Build a dictionary of UTF-8 encodings
+const decodeMapping = {
+  "%20": " ",
+  "%21": "!",
+  "%23": "#",
+  "%24": "$",
+  "%26": "&",
+  "%28": "(",
+  "%29": ")",
+  "%2B": "+",
+  "%5C": "\\"
+};
+
+const encodeMapping = {
+  " ": "%20",
+  "!": "%21",
+  "#": "%23",
+  "$": "%24",
+  "&": "%26",
+  "(": "%28",
+  ")": "%29",
+  "+": "%2B",
+  "\\": "%5C"
+};
 
 /**
  * Decode the URL path to restore the original characters that
@@ -8,9 +30,12 @@
  * @returns {string} The decoded path.
  */
 export function decodeUrl(path) {
-  return path
-    .replaceAll("%20", " ")
-    .replaceAll("%5C", "\\");
+  let decoded = path;
+  Object.keys(decodeMapping).forEach((key => {
+    decoded = decoded.replaceAll(key, decodeMapping[key])
+  }));
+
+  return decoded;
 }
 
 /**
@@ -20,5 +45,10 @@ export function decodeUrl(path) {
  * @return {string} The encoded path.
  */
 export function encodeUrl(path) {
-  return path.replaceAll("\\", "%5C").replaceAll(" ", "%20");
+  let encoded = path;
+  Object.keys(encodeMapping).forEach((key => {
+    encoded = encoded.replaceAll(key, encodeMapping[key])
+  }));
+
+  return encoded;
 }
